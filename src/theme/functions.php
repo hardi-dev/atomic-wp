87eb1674b50785d82695a0df657973dac2958f09
hardiansa <ardititanium@gmail.com>
2020-03-01 16:12:23 +0700

    change function

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -1,6 +1,14 @@
 <?php 
 include_once( get_stylesheet_directory()  . '/vendor/autoload.php' );
 
+function awp_resources() {
+  add_action( 'wp_enqueue_scripts', 'awp_resources' );
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+	wp_enqueue_script( 'header_js', get_template_directory_uri() . '/js/header-bundle.js', null, 1.0, false );
+  wp_enqueue_script( 'footer_js', get_template_directory_uri() . '/js/footer-bundle.js', null, 1.0, true );
+}
+
+
 // Run the theme setup.
 add_filter(
 	'loader_directories',
