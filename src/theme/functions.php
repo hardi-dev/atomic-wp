48668455aab4c8bc7963b1b41a7f449016893efa
hardiansa <ardititanium@gmail.com>
2020-03-01 18:15:39 +0700

    adjust gulpfile can add atomic component

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -1,8 +1,8 @@
 <?php 
 include_once( get_stylesheet_directory()  . '/vendor/autoload.php' );
 
+add_action( 'wp_enqueue_scripts', 'awp_resources' );
 function awp_resources() {
-  add_action( 'wp_enqueue_scripts', 'awp_resources' );
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_script( 'header_js', get_template_directory_uri() . '/js/header-bundle.js', null, 1.0, false );
   wp_enqueue_script( 'footer_js', get_template_directory_uri() . '/js/footer-bundle.js', null, 1.0, true );
